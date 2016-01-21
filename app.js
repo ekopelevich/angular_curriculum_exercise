7ac604eaccdd414093866490af3fe8c9db3ba076
Elana Kopelevich <ekopelevich@gmail.com>
2016-01-20 23:41:11 -0700

    1-7

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// $(document).ready(function(){
-//   console.log('sanity check');
-//   $('.name-submit').click(function(){
-//     var name = $('.name').val();
-//     $('.greeting').append(name);
-//   });
-// });
-
-var app = angular.module('firstApp, ['$sanitize']');
+var app = angular.module('firstApp', []);
 app.controller('MyFirstController', function($scope){
   $scope.name = 'Severus Snape';
+});
+
+app.controller('ExerciseController', function($scope){
+  $scope.favColor = 'blue';
+  $scope.secondsInACentury = 60 * 60 * 24 * 365 * 100;
+  $scope.rightNow = Date.now();
 });
---
+++ madLibsController.js
@@ -0,0 +1,6 @@
+console.log('sanity check');
+
+var app = angular.module('madLibsApp', []);
+app.controller('madLibs', function($scope){
+  
+});
